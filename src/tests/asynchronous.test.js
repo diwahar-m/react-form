276dc26afb076a90314fe378cebe1c42e214bb73
diwahar-m <msdiwahar5@gmail.com>
2025-05-20 13:16:28 +0530

    resolves and rejects test added

--- src/tests/asynchronous.test.js
+++ src/tests/asynchronous.test.js
@@ -3,7 +3,7 @@ import { expect, test } from "vitest";
 // testing async function
 test("testing promise function", ()=> {
     function promiseFun(){
-        return new Promise((res, rej)=> {
+        return new Promise((res)=> {
             return res("success")
         })
     }
@@ -15,7 +15,7 @@ test("testing promise function", ()=> {
 test("testing async await function", async ()=> {
 
     function promiseFun(){
-        return new Promise((res, rej)=> {
+        return new Promise((_res, rej)=> {
             return rej("error")
         })
     }
@@ -25,4 +25,22 @@ test("testing async await function", async ()=> {
     } catch{ err => expect(err).toBe("error")
 
     }
+})
+
+// resolves & rejects 
+test("resolves and rejects method", ()=> {
+    function resolvePromiseFun(){
+        return new Promise((res)=> {
+            return res("success msg")
+        })
+    }
+
+    function rejectPromiseFun(){
+        return new Promise((_res, rej)=> {
+            return rej("error")
+        })
+    }
+
+    expect(resolvePromiseFun()).resolves.toBe("success msg");
+    expect(rejectPromiseFun()).rejects.toMatch("error");
 })
